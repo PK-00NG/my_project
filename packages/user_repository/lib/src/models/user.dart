8f8d915c98bef7e9782a68b1a138c824647bfa38
P.K. <154055330+PK-00NG@users.noreply.github.com>
2024-06-17 16:49:22 +0700

    update

---
+++ packages/user_repository/lib/src/models/user.dart
@@ -0,0 +1,46 @@
+import '../entities/entities.dart';
+
+export 'user.dart';
+
+class MyUser {
+  String userID;
+  String email;
+  String name;
+  bool hasActiveCart;
+
+  MyUser({
+    required this.userID,
+    required this.email,
+    required this.name,
+    required this.hasActiveCart,
+  });
+
+  static final empty = MyUser(
+    userID: '',
+    email: '',
+    name: '',
+    hasActiveCart: false,
+  );
+
+  MyUserEntity toEntity() {
+    return MyUserEntity(
+      userID: userID,
+      email: email,
+      name: name,
+      hasActiveCart: hasActiveCart,
+    );
+  }
+
+  static MyUser frontEntity(MyUserEntity entity) {
+    return MyUser(
+        userID: entity.userID,
+        email: entity.email,
+        name: entity.name,
+        hasActiveCart: entity.hasActiveCart);
+  }
+
+  @override
+  String toString() {
+    return 'MyUser: $userID, $email, $name, $hasActiveCart';
+  }
+}
